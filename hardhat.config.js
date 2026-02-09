89385968d751ebaadf7b78efb5ebe6178288a8fe
Lui Ji <aratheone@proton.me>
2026-02-08 18:33:58 -0600

    Add dotenv support and Base network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.20",
+  networks: {
+    base: {
+      url: "https://mainnet.base.org",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
